4c51846467425ec06c7bce8abde7c99bc1aaed5e
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-05 13:38:53 +0400

    added logic and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useSelector } from "react-redux";
 import "./App.css";
+import  LoginPage  from "./componenets/LoginPage/LoginPage";
 
 function App() {
-  const state = useSelector(state => state.auth);
-  console.log(state)
+  // const state = useSelector(state => state.auth);
+
   return <div className="App">
-    <h1>TEXT</h1>
+    <h1>Вход</h1>
+    <LoginPage/>
   </div>;
 }
 
